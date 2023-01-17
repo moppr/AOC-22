5c74781c527658e40e74132e8ee7dab0ee5b7ee0
moppr <mantony42@gmail.com>
2023-01-17 14:47:34 -0500

    Complete day 1 part 2
    Signed-off-by: moppr <mantony42@gmail.com>

--- 01/01.py
+++ 01/01.py
@@ -1,10 +1,13 @@
 with open("01.in") as f:
     elves = [[int(y) for y in x.split()] for x in f.read().split("\n\n")]
 
-    most = -1
+    top3 = [-1]*3
     for i, elf in enumerate(elves):
         cal = sum(elf)
-        if cal > most:
-            most = cal
+        if cal > top3[0]:
+            top3[0] = cal
+            top3 = sorted(top3)
 
-    print(most)
+    # print(top3)
+    print(top3[2])
+    print(sum(top3))
