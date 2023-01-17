0a1dcd9adee732afb429a893872c4e3c02ba700d
moppr <mantony42@gmail.com>
2023-01-17 14:41:44 -0500

    Complete part 1
    Signed-off-by: moppr <mantony42@gmail.com>

--- 01/01.py
+++ 01/01.py
@@ -1,2 +1,10 @@
 with open("01.in") as f:
-    pass
+    elves = [[int(y) for y in x.split()] for x in f.read().split("\n\n")]
+
+    most = -1
+    for i, elf in enumerate(elves):
+        cal = sum(elf)
+        if cal > most:
+            most = cal
+
+    print(most)
