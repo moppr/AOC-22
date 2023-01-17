ea326cd68bde084414a6b6f8ef9214f2cc93bcde
moppr <mantony42@gmail.com>
2023-01-17 15:19:43 -0500

    Complete day 2 part 2
    Signed-off-by: moppr <mantony42@gmail.com>

--- 02/02.py
+++ 02/02.py
@@ -1,11 +1,14 @@
 with open("02.in") as f:
-    score = 0
+    score1, score2 = 0, 0
     sc1 = {'X': 1, 'Y': 2, 'Z': 3, 'A': 1, 'B': 2, 'C': 3}
-    sc2 = {0: 3, 1: 6, 2: 0}
+    sc2 = {0: 3, 1: 6, 2: 0, 'X': 0, 'Y': 3, 'Z': 6}
+    sc3 = {'X': 2, 'Y': 0, 'Z': 1}
 
     for line in f:
         p1, p2 = line.split()
-        score += sc1[p2]
-        score += sc2[(sc1[p2] - sc1[p1]) % 3]
+        score1 += sc1[p2]
+        score1 += sc2[(sc1[p2] - sc1[p1]) % 3]
+        score2 += (sc1[p1] + sc3[p2] - 1) % 3 + 1
+        score2 += sc2[p2]
 
-    print(score)
+    print(score1, score2)
