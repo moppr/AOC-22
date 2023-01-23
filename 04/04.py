85081829e4936fe7183acc20d456619ebf237df4
moppr <mantony42@gmail.com>
2023-01-23 17:49:09 -0500

    Complete day 4 part 2
    Signed-off-by: moppr <mantony42@gmail.com>

--- 04/04.py
+++ 04/04.py
@@ -1,14 +1,19 @@
 with open("04.in") as f:
 
-    c = 0
+    c1 = c2 = 0
     for pair in f:
         e1, e2 = pair.split(",")
         e1 = [int(x) for x in e1.split("-")]
         e2 = [int(x) for x in e2.split("-")]
 
         if e1[0] <= e2[0] <= e1[1] and e1[0] <= e2[1] <= e1[1]:
-            c += 1
+            c1 += 1
         elif e2[0] <= e1[0] <= e2[1] and e2[0] <= e1[1] <= e2[1]:
-            c += 1
+            c1 += 1
 
-    print(c)
+        if e1[0] <= e2[0] <= e1[1] or e1[0] <= e2[1] <= e1[1]:
+            c2 += 1
+        elif e2[0] <= e1[0] <= e2[1] or e2[0] <= e1[1] <= e2[1]:
+            c2 += 1
+
+    print(c1, c2)
