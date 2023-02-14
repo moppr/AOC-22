919231d521aee8a5e18fd98a17d342c44785abe2
moppr <mantony42@gmail.com>
2023-02-14 15:45:48 -0500

    Complete day 9 part 2
    Signed-off-by: moppr <mantony42@gmail.com>

--- 09/09.py
+++ 09/09.py
@@ -1,6 +1,23 @@
+def update_tail(head_x, head_y, tail_x, tail_y):
+    del_y = head_y - tail_y
+    del_x = head_x - tail_x
+    if abs(del_y) == 2:
+        tail_y += del_y//2
+        if abs(del_x) == 1:
+            tail_x = head_x
+    if abs(del_x) == 2:
+        tail_x += del_x//2
+        if abs(del_y) == 1:
+            tail_y = head_y
+
+    return tail_x, tail_y
+
+
 with open("09.in") as f:
-    head_x = head_y = tail_x = tail_y = 0
-    visited = set()
+    x = [0]*10
+    y = [0]*10
+    visited_1 = set()
+    visited_2 = set()
 
     for line in f:
         instr, dist = line.split()
@@ -9,26 +26,18 @@ with open("09.in") as f:
         for _ in range(dist):
             # update head
             if instr == "U":
-                head_y += 1
+                y[0] += 1
             elif instr == "D":
-                head_y -= 1
+                y[0] -= 1
             elif instr == "R":
-                head_x += 1
+                x[0] += 1
             elif instr == "L":
-                head_x -= 1
+                x[0] -= 1
 
-            # update tail
-            del_y = head_y - tail_y
-            del_x = head_x - tail_x
-            if abs(del_y) == 2:
-                tail_y += del_y//2
-                if del_x != 0:
-                    tail_x = head_x
-            if abs(del_x) == 2:
-                tail_x += del_x//2
-                if del_y != 0:
-                    tail_y = head_y
+            for head in range(9):
+                x[head+1], y[head+1] = update_tail(x[head], y[head], x[head+1], y[head+1])
+            visited_1.add((x[1], y[1]))
+            visited_2.add((x[9], y[9]))
 
-            visited.add((tail_x, tail_y))
-
-    print(len(visited))
+    print(len(visited_1))
+    print(len(visited_2))
