71a5496c89c7488ee3e175de783832f1c3b4283e
moppr <mantony42@gmail.com>
2023-02-21 19:37:47 -0500

    Complete day 11 part 2
    Signed-off-by: moppr <mantony42@gmail.com>

--- 11/11.py
+++ 11/11.py
@@ -2,8 +2,10 @@ with open("11.in") as f:
     monkeys = f.read().split("\n\n")
     monkey_items = {}
     monkey_inspects = {}
+    part = 2
+    stress_reduction = 1
 
-    for round_no in range(21):
+    for round_no in range(21 if part == 1 else 10001):
         for monkey in monkeys:
             monkey_no, starting, operation, test, true_action, false_action = monkey.split("\n")
             monkey_no = int(monkey_no[-2])
@@ -16,15 +18,14 @@ with open("11.in") as f:
             if round_no == 0:
                 monkey_items[monkey_no] = starting
                 monkey_inspects[monkey_no] = 0
+                stress_reduction *= test
                 continue
 
             while monkey_items[monkey_no]:
                 item = monkey_items[monkey_no].pop(0)
-                item = eval(str(item) + operation.replace("old", str(item))) // 3
-                if item % test == 0:
-                    monkey_items[true_action].append(item)
-                else:
-                    monkey_items[false_action].append(item)
+                item = eval(str(item) + operation.replace("old", str(item)))
+                item = (item // 3) if part == 1 else (item % stress_reduction)
+                monkey_items[true_action if item % test == 0 else false_action].append(item)
                 monkey_inspects[monkey_no] += 1
 
     inspects_sorted = sorted(monkey_inspects.values())[-2:]
